a8fa571d8b91cf8fb385f8e2f52e1228a8fecce2
Bipasa-mohapatra <mohapatrabipasa2002@gmail.com>
2024-07-19 10:21:25 +0530

    Add files via upload

---
+++ first project/Frontend-dev/WhileLoops.java
@@ -0,0 +1,15 @@
+public class WhileLoops{
+        public static void main(String[] args) {
+        int a = 20;
+        int i = 0;
+
+        // Corrected loop condition and logic
+        while(i>=a)
+         {
+            System.out.println("Hello World " + i);
+            i--; // Increment i to avoid infinite loop
+        }
+
+        String change = ""; 
+    }
+}
